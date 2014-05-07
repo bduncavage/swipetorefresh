876517d7f10f9246e8b44d10f61e1e710ddfff50
Brett Duncavage <brett.duncavage@rd.io>
2014-05-06 17:13:43 -0700

    csharp: Integrate SwipeRefreshLayout usage into sample app
    The java and csharp samples should appear and function identically now.

--- csharp/SwipeToRefresharpSample/SwipeToRefresharpSample/MainActivity.cs
+++ csharp/SwipeToRefresharpSample/SwipeToRefresharpSample/MainActivity.cs
@@ -5,28 +5,45 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using SwipeToRefresh.Widget;
 
 namespace SwipeToRefresharpSample
 {
-    [Activity(Label = "SwipeToRefresharpSample", MainLauncher = true)]
+    [Activity(Label = "SwipeToRefresharpSample", MainLauncher = true, Theme = "@style/AppTheme")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        Handler handler;
+        SwipeRefreshLayout swipe_refresh_layout;
 
         protected override void OnCreate(Bundle bundle)
         {
+            handler = new Handler();
+
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-			
-            button.Click += delegate {
-                button.Text = string.Format("{0} clicks!", count++);
-            };
+            swipe_refresh_layout = FindViewById<SwipeRefreshLayout>(Resource.Id.swipe_refresh_layout);
+            // Setup the actionbar indicator
+            swipe_refresh_layout.SetActionBarSwipeIndicatorLayout(Resource.Layout.swipe_indicator, Resource.Id.text);
+            swipe_refresh_layout.SetActionBarSwipeIndicatorRefreshingText(Resource.String.loading);
+            swipe_refresh_layout.SetActionBarSwipeIndicatorText(Resource.String.swipe_to_refresh);
+            // Setup colors
+            swipe_refresh_layout.SetColorScheme(
+                Resource.Color.refreshing_color1,
+                Resource.Color.refreshing_color2,
+                Resource.Color.refreshing_color3,
+                Resource.Color.refreshing_color4);
+            // Set the listener
+            swipe_refresh_layout.Refresh += OnRefresh;
+        }
+
+        private void OnRefresh(object sender, EventArgs args)
+        {
+            handler.PostDelayed(() => {
+                swipe_refresh_layout.Refreshing = false;
+            }, 3000);
         }
     }
 }
